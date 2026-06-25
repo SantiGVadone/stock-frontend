bd1d0a1260afd4ad254bf6495bec0a17e8e0f016
SantiGVadone <santiagovadone36@gmail.com>
2026-06-25 17:16:23 -0300

    Add functions for stores managment

--- src/utility/auth.ts
+++ src/utility/auth.ts
@@ -1,5 +1,6 @@
 import * as SecureStore from 'expo-secure-store'
 const TOKEN_KEY = 'user_session_token'
+const STORE_KEY = 'user_stores'
 
 export const guardarToken = async (token: string) => {
   try {
@@ -26,5 +27,43 @@ export const eliminarToken = async () => {
     console.log('Token eliminado')
   } catch (error) {
     console.error('Error al eliminar el token:', error)
+  }
+}
+
+export interface Store {
+  id: number
+  name: string
+  rol: string
+}
+
+export const guardarStores = async (stores: Store[]): Promise<void> => {
+  try {
+    const storesString = JSON.stringify(stores)
+    await SecureStore.setItemAsync(STORE_KEY, storesString)
+    console.log('Stores guardadas con éxito')
+  } catch (error) {
+    console.error('Error al guardar las stores:', error)
+  }
+}
+
+export const obtenerStores = async (): Promise<Store[] | null> => {
+  try {
+    const storesString = await SecureStore.getItemAsync(STORE_KEY)
+    if (storesString) {
+      return JSON.parse(storesString) as Store[]
+    }
+    return null
+  } catch (error) {
+    console.error('Error al obtener las stores:', error)
+    return null
+  }
+}
+
+export const eliminarStores = async (): Promise<void> => {
+  try {
+    await SecureStore.deleteItemAsync(STORE_KEY)
+    console.log('Stores eliminadas')
+  } catch (error) {
+    console.error('Error al eliminar las stores:', error)
   }
 }
