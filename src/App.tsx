4c06b259cc567325c85af01ab9ed476ec98d3087
SantiGVadone <santiagovadone36@gmail.com>
2026-07-08 17:54:02 -0300

    provide App with context

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 import { Navigation } from './navigation/Navigation'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
+import { AuthProvider } from './context/AuthContext'
 
 export function App() {
   return (
     <GestureHandlerRootView>
       <SafeAreaProvider>
-        <Navigation />
+        <AuthProvider>
+          <Navigation />
+        </AuthProvider>
       </SafeAreaProvider>
     </GestureHandlerRootView>
   )
