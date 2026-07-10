693dc438438ae24f9d3c67c70e26710a7963c30d
SantiGVadone <santiagovadone36@gmail.com>
2026-07-10 00:24:44 -0300

    add hook for stores

---
+++ src/hooks/useStore.tsx
@@ -0,0 +1,63 @@
+import { useState } from 'react'
+import { useAuth } from '../context/AuthContext'
+
+const API_URL = 'https://api.vadonedev.com.ar/api'
+
+interface Store {
+  id: number
+  name: string
+  location: string
+  phone: number
+}
+
+export interface AddNewStore {
+  name: string
+  location: string
+  phone: string
+}
+
+export const useStore = () => {
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState<string | null>(null)
+  const { token, addStore } = useAuth()
+
+  const newStore = async (store: AddNewStore) => {
+    setLoading(true)
+    if (!store.name || !store.location || !store.phone) {
+      alert('Todos los campos son obligatorios.')
+      setLoading(false)
+      return
+    }
+    try {
+      setLoading(true)
+      const response = await fetch(`${API_URL}/stores`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+          name: store.name,
+          location: store.location,
+          phone: store.phone,
+        }),
+      })
+      if (!response.ok) {
+        const errorDetail = await response.text()
+        setError(errorDetail)
+        console.error('Error al guardar en la base de datos', errorDetail)
+      } else {
+        const createdStore = await response.json()
+        addStore(createdStore)
+      }
+    } catch (error) {
+      console.error(error)
+      alert('No se pudo crear la Tienda')
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  return { newStore, loading, error }
+}
