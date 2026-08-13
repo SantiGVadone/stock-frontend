b067b257b0a75830ef7664e744b2c23678cbc1af
SantiGVadone <santiagovadone36@gmail.com>
2026-08-13 17:52:58 -0300

    add possible solution

--- src/navigation/RequireAuth.tsx
+++ src/navigation/RequireAuth.tsx
@@ -7,6 +7,35 @@ export const RequireAuth = ({
   children,
   ...props
 }: { children: React.ReactNode } & Record<string, any>) => {
+  /* POSIBLE SOLUCION AL LOGOUT ERROR
+  const { isAuthenticated, isLoading } = useAuth()
+  const navigation = useNavigation<any>()
+
+  // 1. Manejamos la redirección como un efecto secundario
+  useEffect(() => {
+    if (!isLoading && !isAuthenticated) {
+      navigation.navigate('Login')
+    }
+  }, [isLoading, isAuthenticated, navigation])
+
+  // 2. Si está cargando, mostramos el spinner
+  if (isLoading) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ActivityIndicator size='large' color='#0061D9' />
+      </View>
+    )
+  }
+
+  // 3. Si no está autenticado, no renderizamos el contenido (esperamos que el useEffect redirija)
+  if (!isAuthenticated) {
+    return null
+  }
+
+  return Children.map(children, (child) =>
+    isValidElement(child) ? cloneElement(child, props) : child,
+  )
+  */
   const { isAuthenticated, isLoading } = useAuth()
   const navigation = useNavigation<any>()
 
